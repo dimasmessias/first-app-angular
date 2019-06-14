88ed315427476cf2612e4afb995f188465406607
Dimas Messias <dimas_messias@hotmail.com>
2019-06-14 23:20:58 +0200

    updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-   selector: 'app-header',
-   templateUrl: './header.component.html',
-   styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {
-   @Output() featureSelected = new EventEmitter<string>();
+	@Output() public featureSelected = new EventEmitter<string>();
 
-   onSelect(feature: string) {
-      this.featureSelected.emit(feature);
-   }
+	public onSelect(feature: string) {
+		this.featureSelected.emit(feature);
+	}
 
 }
