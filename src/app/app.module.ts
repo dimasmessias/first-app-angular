a5f69a9cb8cd20c6a02ad1e3d5e03e3a5b7fe01f
Dimas Messias <dimas_messias@hotmail.com>
2019-07-18 20:25:52 +0200

    Added shopping module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,32 +10,35 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeService } from './recipes/recipe.services';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { AlertComponent } from './shared/alert/alert.component';
 import { DataStorageService } from './shared/data-storage.service';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { LoginModePipe } from './shared/login-mode.pipe';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListService } from './shopping-list/shopping-list.services';
+import { ShoppingModule } from './shopping-list/shopping.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		ShoppingListComponent,
-		ShoppingEditComponent,
 		DropdownDirective,
-		RecipesStartComponent,
 		FooterComponent,
 		AuthComponent,
 		LoginModePipe,
 		AlertComponent,
 		PlaceholderDirective
 	],
-	imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule, RecipesModule],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		AppRoutingModule,
+		RecipesModule,
+		ShoppingModule
+	],
 	providers: [ShoppingListService, RecipeService, DataStorageService, RecipesResolverService, {
 		provide: HTTP_INTERCEPTORS,
 		useClass: AuthInterceptorService,
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,6 +6,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesStartComponent } from './recipes-start/recipes-start.component';
 import { RecipesComponent } from './recipes.component';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { RecipesComponent } from './recipes.component';
 		RecipeListComponent,
 		RecipeDetailComponent,
 		RecipeItemComponent,
-		RecipeEditComponent
+		RecipeEditComponent,
+		RecipesStartComponent
 	],
 	imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
