a7b03137bb0d4ec52e522cea145b035886912f0d
Dimas Messias <dimas_messias@hotmail.com>
2019-07-18 18:54:58 +0200

    Added recipes related modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,10 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthComponent } from './auth/auth.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeService } from './recipes/recipe.services';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesModule } from './recipes/recipes.module';
 import { AlertComponent } from './shared/alert/alert.component';
 import { DataStorageService } from './shared/data-storage.service';
 import { DropdownDirective } from './shared/dropdown.directive';
@@ -29,22 +25,17 @@ import { ShoppingListService } from './shopping-list/shopping-list.services';
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		RecipesComponent,
-		RecipeListComponent,
-		RecipeDetailComponent,
-		RecipeItemComponent,
 		ShoppingListComponent,
 		ShoppingEditComponent,
 		DropdownDirective,
 		RecipesStartComponent,
-		RecipeEditComponent,
 		FooterComponent,
 		AuthComponent,
 		LoginModePipe,
 		AlertComponent,
 		PlaceholderDirective
 	],
-	imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule],
+	imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule, RecipesModule],
 	providers: [ShoppingListService, RecipeService, DataStorageService, RecipesResolverService, {
 		provide: HTTP_INTERCEPTORS,
 		useClass: AuthInterceptorService,
@@ -53,6 +44,4 @@ import { ShoppingListService } from './shopping-list/shopping-list.services';
 	bootstrap: [AppComponent],
 	entryComponents: [AlertComponent]
 })
-export class AppModule
-{
-}
+export class AppModule {}
